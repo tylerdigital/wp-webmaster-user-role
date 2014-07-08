a797a987f42a835734644a9f0aec6c882dca3b62
Nathan Tyler <croixhaug@gmail.com>
2014-07-07 22:27:23 -0700

    Create role if missing

--- webmaster-user-role.php
+++ webmaster-user-role.php
@@ -62,6 +62,7 @@ if ( !class_exists( 'TD_WebmasterUserRole' ) ) {
 			add_action( 'updated_'.self::slug.'_option', array( $this, 'updated_option' ), 10, 3 );
 			add_action( 'deleted_'.self::slug.'_option', array( $this, 'deleted_option' ) );
 			add_action( 'admin_menu', array( &$this, 'admin_menu' ), 999 );
+			add_action( 'admin_init', array( &$this, 'create_role_if_missing' ), 10 );
 			add_action( 'admin_init', array( &$this, 'cleanup_dashboard_widgets' ), 20 );
 			$site_version = get_site_option( 'td-webmaster-user-role-version' );
 			if( $site_version!=self::version ) {
@@ -206,6 +207,14 @@ if ( !class_exists( 'TD_WebmasterUserRole' ) ) {
 			}
 
 			return $capabilities;
+		}
+
+		function create_role_if_missing() {
+			$wp_roles = new WP_Roles();
+			if ( $wp_roles->is_role( 'webmaster' ) ) return;
+
+			$this->deactivate( false );
+			$this->activate( false );
 		}
 
 		function cleanup_dashboard_widgets() {
