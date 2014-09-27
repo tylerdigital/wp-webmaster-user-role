6c71f7cbdaa04482167b5dd7d453eec6a5e69950
Nathan Tyler <croixhaug@gmail.com>
2014-09-27 16:58:23 -0700

    Add support for Total theme

---
+++ includes/themes/total-theme-module.php
@@ -0,0 +1,55 @@
+<?php
+class TDWUR_Total_Theme {
+	private $active;
+
+	function __construct() {
+		$this->parent = $parent;
+
+		add_filter( 'webmaster_supported_theme', array( $this, 'is_active' ) );
+		add_filter( 'webmaster_supported_theme_setting_fields', array( $this, 'setting_fields' ) );
+
+	}
+
+	function is_active() {
+		if ( $this->active ) return true;
+
+		$current_theme = wp_get_theme();
+		if ( $current_theme->Name=='Total' || $current_theme->Template=='Total' ) {
+			$this->active = true;
+			add_action( 'admin_menu', array( $this, 'admin_menu' ), 100 );
+
+			return true;
+		}
+
+		return false;
+	}
+
+	function setting_fields() {
+		$fields = array();
+		$fields[] = array(
+			'id'        => 'total_theme_settings',
+			'type'      => 'checkbox',
+			'title'     => __('Total Theme Compatibility', 'webmaster-user-role'),
+			'subtitle'  => __('Webmaster users can', 'webmaster-user-role'),
+
+			'options'   => array(
+				'access_theme_options_panel' => 'Access Theme Options panel',
+			),
+
+			'default'   => array(
+				'access_theme_options_panel' => '0',
+			)
+		);
+
+		return $fields;
+	}
+
+	function admin_menu() {
+		global $webmaster_user_role_config;
+		if ( !is_array( $webmaster_user_role_config ) ) return;
+
+		if ( empty( $webmaster_user_role_config['total_theme_settings']['access_theme_options_panel'] ) ) remove_menu_page( 'wpex_options' );
+	}
+
+}
+new TDWUR_Total_Theme();
